d1f1efc8715512c9b394b600f392ba16f4abc72a
Kirill <pechkirill@gmail.com>
2019-04-13 15:40:51 +0000

    Ref mocks

--- src/blog/tests_utils/utils.py
+++ src/blog/tests_utils/utils.py
@@ -2,19 +2,13 @@ from unittest.mock import MagicMock, patch
 
 from django.core.files import File
 
-FILE_MOCK = MagicMock(File, name='FileMock')
-
-
-def new_save(*args, **kwargs):
-    return args[1]
-
 
 def patch_storage(func):
-    return patch(
-        'django.core.files.storage.FileSystemStorage.save',
-        new=new_save
-    )(func)
+    def new_save(name, *args, **kwargs):
+        return name
+
+    return patch('django.core.files.storage.FileSystemStorage.save', MagicMock(side_effect=new_save))(func)
 
 
 def patch_file(func):
-    return patch('django.core.files.File', FILE_MOCK)(func)
+    return patch('django.core.files.File', MagicMock(File))(func)
