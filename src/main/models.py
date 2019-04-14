15f13a4c1843284cfe349434125b8c85c5af7b54
Kirill <pechkirill@gmail.com>
2019-04-14 12:04:32 +0000

    Add indexes

--- src/main/models.py
+++ src/main/models.py
@@ -24,8 +24,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, CASCADE, primary_key=True, verbose_name=_('user'))
-    first_name = models.CharField(_('first_name'), max_length=16, blank=True)
-    last_name = models.CharField(_('last_name'), max_length=32, blank=True)
+    first_name = models.CharField(_('first_name'), max_length=16, blank=True, db_index=True)
+    last_name = models.CharField(_('last_name'), max_length=32, blank=True, db_index=True)
     wallpaper = models.ImageField(_('wallpaper'), upload_to=UploadToFactory('main/profile/wallpaper'), null=True)
     avatar = models.ImageField(_('avatar'), upload_to=UploadToFactory('main/profile/avatar'), null=True)
 
@@ -38,10 +38,10 @@ class Post(models.Model):
     author = models.ForeignKey(Profile, CASCADE, related_name='posts', verbose_name=_('author'))
     tags = models.ManyToManyField('Tag', related_name='posts', verbose_name=_('tags'))
 
-    title = models.CharField(_('title'), max_length=64)
+    title = models.CharField(_('title'), max_length=64, db_index=True)
     image = models.ImageField(_('image'), upload_to=UploadToFactory('main/post/image'))
     text = models.TextField(_('text'))
-    created_at = models.IntegerField(_('created at timestamp'), default=time, editable=False)
+    created_at = models.IntegerField(_('created at timestamp'), default=time, editable=False, db_index=True)
 
 
 class Comment(models.Model):
@@ -51,4 +51,4 @@ class Comment(models.Model):
 
 
 class Tag(models.Model):
-    title = models.CharField(_('title'), max_length=16)
+    title = models.CharField(_('title'), max_length=16, unique=True)
