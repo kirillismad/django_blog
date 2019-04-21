0256af4d7375cfd97825a14d7db516ce07d50e35
Kirill <pechkirill@gmail.com>
2019-04-21 12:41:51 +0000

    ref views to generics

--- src/main/views.py
+++ src/main/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views import View
-
+from django.views import generic
 from main.forms import SignUpForm, SignInForm, CommentForm, PostForm, ProfileUpdateForm
 from main.models import Post, Profile, Tag, Comment
 
@@ -40,17 +40,32 @@ class MainView(View):
         if form.is_valid():
             post = form.save(commit=False)
             post.author_id = request.user.pk
-            form.save()
+            post.save()
+            form.save_m2m()
             return redirect(post)
         return redirect('main:root')
 
 
-class PostDetailView(View):
-    def get(self, request, id):
-        form = CommentForm()
+class PostDetailView(generic.DetailView):
+    pk_url_kwarg = 'id'
+    template_name = 'main/post_detail.html'
+
+    def get_queryset(self):
         pfr_comments = Prefetch('comments', Comment.objects.select_related('author').order_by('-created_at'))
-        post = get_object_or_404(Post.objects.select_related('author').prefetch_related('tags', pfr_comments), pk=id)
-        return render(request, 'main/post_detail.html', context={'post': post, 'user': request.user, 'form': form})
+        return Post.objects.select_related('author').prefetch_related('tags', pfr_comments)
+
+    def get_context_data(self, **kwargs):
+        return {
+            'post': self.object,
+            'user': self.request.user,
+            'form': CommentForm()
+        }
+
+    # def get(self, request, id):
+    #     form = CommentForm()
+    #     pfr_comments = Prefetch('comments', Comment.objects.select_related('author').order_by('-created_at'))
+    #     post = get_object_or_404(Post.objects.select_related('author').prefetch_related('tags', pfr_comments), pk=id)
+    #     return render(request, 'main/post_detail.html', context={'post': post, 'user': request.user, 'form': form})
 
 
 class CommentView(View):
@@ -105,22 +120,48 @@ class SingOut(View):
         return redirect(reverse('main:root'))
 
 
-class ProfileView(View):
-    def get(self, request):
-        profiles = Profile.objects.annotate(posts_count=Count('posts'))
+class ProfileView(generic.ListView):
+    queryset = Profile.objects.annotate(posts_count=Count('posts'))
+    template_name = 'main/profiles.html'
 
-        q = request.GET.get('q')
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        q = self.request.GET.get('q')
         if q is not None:
-            profiles = profiles.filter(Q(first_name__icontains=q) | Q(last_name__icontains=q))
-        return render(request, 'main/profiles.html', context={'profiles': profiles, 'user': request.user})
+            queryset = queryset.filter(Q(first_name__icontains=q) | Q(last_name__icontains=q))
+
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        return {
+            'profiles': self.object_list, 'user': self.request.user
+        }
+
+    # def get(self, request):
+    #     profiles = Profile.objects.annotate(posts_count=Count('posts'))
+    #
+    #     q = request.GET.get('q')
+    #     if q is not None:
+    #         profiles = profiles.filter(Q(first_name__icontains=q) | Q(last_name__icontains=q))
+    #     return render(request, 'main/profiles.html', context={'profiles': profiles, 'user': request.user})
 
 
-class ProfileDetailView(View):
-    def get(self, request, id):
+class ProfileDetailView(generic.DetailView):
+    template_name = 'main/profile_detail.html'
+    pk_url_kwarg = 'id'
+
+    def get_queryset(self):
         pfr_posts = Prefetch('posts', Post.objects.annotate(comments_count=Count('comments')))
-        profile = get_object_or_404(Profile.objects.prefetch_related(pfr_posts), pk=id)
+        return Profile.objects.prefetch_related(pfr_posts)
 
-        return render(request, 'main/profile_detail.html', context={'profile': profile, 'user': request.user})
+    def get_context_data(self, **kwargs):
+        return {'profile': self.object, 'user': self.request.user}
+
+    # def get(self, request, id):
+    #     pfr_posts = Prefetch('posts', Post.objects.annotate(comments_count=Count('comments')))
+    #     profile = get_object_or_404(Profile.objects.prefetch_related(pfr_posts), pk=id)
+    #
+    #     return render(request, 'main/profile_detail.html', context={'profile': profile, 'user': request.user})
 
 
 @method_decorator(login_required, 'dispatch')
@@ -146,13 +187,23 @@ class ProfileUpdateView(View):
         return render(request, 'main/profile_update.html', context={'form': form, 'user': request.user})
 
 
-class TagsView(View):
-    def get(self, request):
-        tags = Tag.objects.all()
-        return render(request, 'main/tags.html', context={'tags': tags, 'user': request.user})
+class TagsView(generic.ListView):
+    queryset = Tag.objects.all()
+    template_name = 'main/tags.html'
+
+    # def get(self, request):
+    #     tags = Tag.objects.all()
+    #     return render(request, 'main/tags.html', context={'tags': tags, 'user': request.user})
+
+    def get_context_data(self, *args, **kwargs):
+        return {'tags': self.object_list, 'user': self.request.user}
 
 
 @method_decorator(login_required, 'dispatch')
-class ProfileSelfView(View):
-    def get(self, request):
-        return redirect('main:profiles_detail', id=request.user.pk)
+class ProfileSelfView(generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('main:profiles_detail', kwargs={'id': self.request.user.pk})
+
+    # def get(self, request):
+    #     return redirect('main:profiles_detail', id=request.user.pk)
